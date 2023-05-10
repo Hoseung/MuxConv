7c7ba563fa892664f57df0a7233db370fd999243
Hoseung Choi <hopung@gmail.com>
2023-05-10 20:19:25 +0900

    Testing Comparator

--- muxcnn/resnet_HEAAN.py
+++ muxcnn/resnet_HEAAN.py
@@ -272,8 +272,6 @@ class ResNetHEAAN():
         q = get_q(co,pi)
         fh,fw= kernels[0],kernels[1]
         print(f"[MultParConv] Layer structure: (hi,wi,ci,ki,ti,pi) =({hi:2},{wi:2},{ci:2},{ki:2},{ti:2}, {pi:2})")
-        #print(f"[MultParConv] (ho,wo,co,ko,to,po) =({ho:2},{wo:2},{co:2},{ko:2},{to:2}, {po:2})")
-        #print(f"[MultParConv] q = {q}")
 
         MuxBN_C, MuxBN_M, MuxBN_I = parMuxBN(bn_layer, outs, nslots)
 
