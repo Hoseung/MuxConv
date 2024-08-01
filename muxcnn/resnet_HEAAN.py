34b7c639335e174219a05bb08df72cad97c9b015
Hoseung <hschoi@dinsight.ai>
2024-08-01 21:40:43 +0900

    comments

--- muxcnn/resnet_HEAAN.py
+++ muxcnn/resnet_HEAAN.py
@@ -40,9 +40,11 @@ class ResNetHEAAN():
         if self.debug:
             self._activation_org = relu
         else:
+            # [0801] ApprRelu_HEAAN 대신 2차 Hermite Polynomial 사용. (ReLU 대신 2차함수)
             self.activation = ApprRelu_HEAAN(self.hec, eps=eps, margin=margin, *args, **kwargs)
 
     def forward(self, ctxt, ki=1, hi=32, wi=32, debug=None, verbose=True):
+        # [0801] FHE 연산을 직접 사용하지 않는 부분이므로 수정 필요 없음 (Her 버전에서도 동일)
         if debug is None: debug = self.debug
         t0 = time()
         if verbose: print("[FHE_CNN] Inference started...")
@@ -73,6 +75,7 @@ class ResNetHEAAN():
         return result
 
     def pack_img_ctxt(self, img_tensor):
+        # [0801] encrypt하기 전까진 CTXT가 아니므로 FHE backend와 무관
         """ For convenience
         """
         model = self.torch_model
@@ -83,9 +86,11 @@ class ResNetHEAAN():
         # early conv and bn
         _, ins0, outs0 = get_conv_params(model.conv1, {'k':ki, 'h':hi, 'w':wi})
         ct_a = MultParPack(imgl, ins0)
+        # [0801] encrypt를 OFHE로 변경
         return self.hec.encrypt(ct_a)
 
     def forward_early(self, ct_a, ki, hi, wi, debug=False, verbose=True):
+        # [0801] 여기도 debug부분 말고는 모두 high-level이어서 FHE 백엔드와 무관
         model = self.torch_model
         _, ins0, outs0 = get_conv_params(model.conv1, {'k':ki, 'h':hi, 'w':wi})
 
@@ -109,16 +114,16 @@ class ResNetHEAAN():
     def forward_bb(self, bb:ResNet20.BasicBlock, ctxt_in, outs_in, debug=False, verbose=True):
         # Bootstrap before shortcut
         if ctxt_in.logq <= 80:
-            ctxt_in = self.hec.bootstrap2(ctxt_in)
+            ctxt_in = self.hec.bootstrap2(ctxt_in) # [0801] Ofhe에서는 bootstrapping 자동 모드가 있으므로 HEAAN의 bootstrapping은 모두 무시
             if debug: print("MuxBN bootstrap", ctxt_in.logp, ctxt_in.logq)
 
-        shortcut = he.Ciphertext(ctxt_in)
+        shortcut = he.Ciphertext(ctxt_in) # [0801] 원래는 self.hec.copy(ctxt_in)이어야 올바름.
 
-        _, ins, outs = get_conv_params(bb.conv1, outs_in)
+        _, ins, outs = get_conv_params(bb.conv1, outs_in) # [0801] FHE backend와 무관 -- 파라미터 모양이 복잡 -> Lee.2022의 알고리즘
         t0 = time() 
         if verbose: print("[FHE_CNN BasicBlock] ConvBN1 started...")
         ctxt = self.forward_convbn_par_fhe(bb.conv1,
-                                        bb.bn1, ctxt_in, ins)
+                                        bb.bn1, ctxt_in, ins) # [0801] 들어가면 FHE계산 있음. function signature는 그대로 유지해도 됨
         if verbose: print("[FHE_CNN BasicBlock] ConvBN1 finished in {:.2f} sec".format(time()-t0))
         t0 = time()
         if verbose: print("[FHE_CNN BasicBlock] ReLU1 started...")
@@ -127,7 +132,7 @@ class ResNetHEAAN():
             activated = self._activation_org(tmp)
             ctxt = self.hec.encrypt(activated)
         else:
-            ctxt = self.activation(ctxt)    
+            ctxt = self.activation(ctxt) # [0801] 통째로 Her 버전으로 대체
         #ctxt = self.activation(ctxt)
         if verbose: print("[FHE_CNN BasicBlock] ReLU1 finished in {:.2f} sec".format(time()-t0))
         if debug: 
@@ -157,6 +162,9 @@ class ResNetHEAAN():
             print("[forward_bb] Shortcut", shortcut)
             print("ctxt", ctxt)
 
+        # [0801] HEAAN에서는 CTXT의 스케일을 자동으로 관리해주지 않아서 다른 루트로 온 CTXT를 더하기 전에
+        # 스케일 맞춰주는 과정이 필요.  (rescale, mat_mod_
+        # OFHE에서는 신경 안 써도 됨. 
         # Add shortcut
         if ctxt.logp > shortcut.logp:
             if debug: print("ctxt > shortcut")
@@ -174,14 +182,16 @@ class ResNetHEAAN():
             print(self.hec.decrypt(ctxt))
             print(self.hec.decrypt(shortcut))
         
-        self.hec.add(ctxt, shortcut, inplace=True)
+        # [0801] OFHE버전의 'wrapper'에 add를 구현 하면 됨.
+        # [0801] Add나 Mult의 inplace 옵션을 유지하는게 유리함. 
+        self.hec.add(ctxt, shortcut, inplace=True) 
         #pickle.dump(self.hec.decrypt(ctxt), open("ctxt4.pkl", "wb"))
         #ctxt += shortcut
         t0 = time()
         if verbose: print("[FHE_CNN BasicBlock] ReLU2 started...")
         # Activation
         if debug: 
-            tmp = self.hec.decrypt(ctxt)
+            tmp = self.hec.decrypt(ctxt) 
             activated = self._activation_org(tmp)
             ctxt = self.hec.encrypt(activated)
         else:
@@ -204,14 +214,17 @@ class ResNetHEAAN():
             #ctxt += np.roll(ctxt, 2**i*ni)
             hec.add(ctxt, 
                 hec.lrot(ctxt, -2**i*ni, inplace=False),
-                        inplace=True)
+                        inplace=True) # [0801] Rotation도 OFHE버전의 wrapper에 구현
             
 
         # multiply 64 * 10 at once
         # AVGPool에서 S_vec를 조절하면 이 단계의 일부를 미리 수행할 수 있음 !!
         #ctxt = weight_vec * ctxt
-        hec.multByVec(ctxt, weight_vec, inplace=True)
-        hec.rescale(ctxt)
+        # [0801] ctxt * np.array 연산
+        hec.multByVec(ctxt, weight_vec, inplace=True) 
+        # [0801] OFHE버전의 wrapper에 구현
+        # [0801] 어쩌면 OFHE에서 Polymorphism을 지원해서, 그냥 ndarray로 넣어도 알아서 처리할 수 있을지도.
+        hec.rescale(ctxt) # [0801] Rescale은 무시
 
         # Sum 64 numbers each 
         for j in range(int(np.log2(ni))):
@@ -232,6 +245,7 @@ class ResNetHEAAN():
 
 
     def SumSlots(self, ct_a,m,p):
+        # FHE_Simulator (hemul)에서 Numpy버전 참고하여 OFHE로 작성
         """Addition only"""
         ev = self.hec
         nrots = 0
@@ -265,8 +279,8 @@ class ResNetHEAAN():
                         scale_factor=1, debug=False):
         """Consumes two mults"""
         if ct_a.logq <= 80:
-            ct_a = self.hec.bootstrap2(ct_a)
-        ev = self.hec
+            ct_a = self.hec.bootstrap2(ct_a) #[0801] 무시
+        ev = self.hec # [0801] 이 부분 기억!
 
         #encoder = self.hec
         hi,wi,ci,ki,ti,pi = [ins[k] for k in ins.keys()]
@@ -279,7 +293,7 @@ class ResNetHEAAN():
 
         ct_d = self.gen_new_ctxt() ####
         
-        ev.modDownTo(ct_d, ct_a.logq - 2*ct_d.logp)
+        ev.modDownTo(ct_d, ct_a.logq - 2*ct_d.logp) # [0801] OFHE에서는 무시
         if debug: print("ct_d", ct_d.logp, ct_d.logq)
         ct = []
         nrots=0
@@ -301,9 +315,9 @@ class ResNetHEAAN():
                     w = ParMultWgt(U,i1,i2,i3,ins,co,kernels,nslots)
                     tmp = ev.multByVec(ct[i1][i2], w, inplace=False)
                     ev.rescale(tmp)
-                    ev.add(ct_b, tmp, inplace=True) ####
+                    ev.add(ct_b, tmp, inplace=True) # [0801] ev = HEAAN.evaluator == self.hec랑 같음.
             
-            ct_c,nrots0 = self.SumSlots(ct_b, ki,              1)
+            ct_c,nrots0 = self.SumSlots(ct_b, ki,              1) #[0801] numpy버전 참고해서 OFHE로 작성되어있을 것으로..
             ct_c,nrots1 = self.SumSlots(ct_c, ki,          ki*wi)
             ct_c,nrots2 = self.SumSlots(ct_c, ti,  (ki**2)*hi*wi)
             nrots += nrots0 + nrots1 + nrots2#____________________________________ROTATION
@@ -335,7 +349,10 @@ class ResNetHEAAN():
         
         plain_vec = -1/scale_factor*(MuxBN_C*MuxBN_M-MuxBN_I)
                                         #,ct_d.logp)
-        ev.addConst(ct_d, plain_vec, inplace=True)
+        ev.addConst(ct_d, plain_vec, inplace=True) 
+        # [0801] multByVen처럼 OFHE에서는 바로 지원할 수도 있음.
+        # [0801] 말하자면 add Vec임. 
+
 
         return ct_d
     
@@ -345,13 +362,14 @@ class ResNetHEAAN():
 
     def AVGPool(self, ct_in, ins, nslots, verbose=True):
         hec = self.hec
-        ct_a = he.Ciphertext(ct_in)
+        ct_a = he.Ciphertext(ct_in) # [0801] 수정 필요 -> hec.copy()
         #ct_b = np.zeros(nslots)
-        ct_b = self.gen_new_ctxt()
+        ct_b = self.gen_new_ctxt() # [0801] hec.new_ctxt()? 
         hi,wi,ci,ki,ti,pi = [ins[k] for k in ins.keys()]
 
         # 한 페이지에 분포하는 64개 숫자를 더함
         for j in range(int(np.log2(wi))):
+            # [0801] forward_linear에서 이미 해결했음
             hec.add(ct_a, 
                 hec.lrot(ct_a, 2**j*ki, inplace=False),
                         inplace=True)
@@ -363,6 +381,7 @@ class ResNetHEAAN():
                 hec.lrot(ct_a, 2**j*ki*ki*wi, inplace=False),
                         inplace=True) 
 
+        # [0801] OFHE에서는 무시
         hec.modDownTo(ct_b, ct_a.logq - ct_a.logp)
         ### + 64채널에서 하나씩을 뽑아옴.
         for i1 in range(ki): # 4
@@ -371,7 +390,7 @@ class ResNetHEAAN():
                 #ct_b += np.roll(ct_a, -(ki**2*hi*wi*i2 + ki*wi*i1 - ki*(ki*i2+i1)))* S_vec
                 tmp = hec.lrot(ct_a, (ki**2*hi*wi*i2 + ki*wi*i1 - ki*(ki*i2+i1)), inplace=False)
                 hec.multByVec(tmp, S_vec, inplace=True)
-                hec.rescale(tmp)
+                hec.rescale(tmp) # [0801] 무시
                 hec.add(ct_b, tmp, inplace=True) 
                 
         return ct_b
